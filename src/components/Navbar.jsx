85341f0de16b74445b8d17944d3893e9e3446fd2
Belghomari Abdelmalek <belghomariabdelmalek@gmail.com>
2025-06-09 22:34:26 +0200

    the webstite home page is almost done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../theme";
+import NightModeSwitcher from "./NightModeSwitcher";
 
 function Navbar() {
+
+  const { theme } = useTheme();
   return (
-    <div className="text-white p-4">
+    <div className={`${theme.textColor} p-4`}>
       <nav>
         <div className="flex flex-col md:flex-row md:items-center md:justify-between max-w-7xl mx-auto space-y-4 md:space-y-0">
-          {/* Name */}
-          <Link to="/" className="text-4xl font-signatra text-center md:text-left">
-            Abdelmalek Belghomari
-          </Link>
+          <div className="flex items-center md:justify-start">
+            <div className="flex-1 md:flex-none pl-10">
+              <Link to="/" className="text-4xl font-signatra text-center md:text-left block ">
+                Abdelmalek Belghomari
+              </Link>
+            </div>
+            <div className="block md:hidden flex-shrink-0">
+              <NightModeSwitcher />
+            </div>
+            
+          </div>
 
-          {/* Menu Items */}
-          <ul className="flex flex-row justify-between px-20 md:flex-row md:space-y-0 md:space-x-8">
+          <ul className="flex flex-row justify-center space-x-4 md:flex-row md:space-y-0 px-10 md:space-x-8 items-center">
             <li>
               <Link to="/" className="">
                 Projects
@@ -26,6 +36,9 @@ function Navbar() {
               <Link to="/contact" className="">
                 Contact
               </Link>
+            </li>
+            <li className="hidden md:block">
+              <NightModeSwitcher />
             </li>
           </ul>
         </div>
